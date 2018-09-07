1f9855dda9e9a24ed238b6700980b967c04bb1d7
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-09-07 23:17:18 +0530

    locale support added

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+const Alexa = require('ask-sdk');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+// locale finder code snippet
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+      resources: languageStrings,
+      returnObjects: true
+    });
+
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function (...args) {
+      return localizationClient.t(...args);
+    };
+  },
+};
+
 exports.handler = skillBuilder
   .addRequestHandlers(
     HelpHandler,
     RepeatHandler,
-    ExitHandler,
-    SessionEndedRequestHandler
+    ExitHandler
   )
+  .addRequestInterceptors(LocalizationInterceptor)
   .lambda();
